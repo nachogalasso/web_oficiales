ed16d81d57f525f5ea9f361778985248a61b338f
nachogalasso <ignaciogalasso82@gmail.com>
2023-04-12 21:10:05 +0200

    dashboard and calendar

--- oficiales/models.py
+++ oficiales/models.py
@@ -2,6 +2,8 @@ from unicodedata import category
 from django.db import models
 
 # Create your models here.
+
+# OFICIALES FAARG
 class Oficiales(models.Model):
     name = models.CharField(max_length=150, null=True)
     email = models.CharField(max_length=150, null=True)
@@ -10,6 +12,8 @@ class Oficiales(models.Model):
     def __str__(self):
         return self.name
     
+
+# INSCRIPCIONES AL CURSO DE ARBITRAJE
 class NewRecruits(models.Model):
     SOCIALMEDIA = (
         ('Web FAA', 'Web FAA'),
@@ -49,12 +53,41 @@ class NewRecruits(models.Model):
         return '{} {}'.format(self.surname, self.name) 
     
     
+# DISPONIBILIDAD 
 class Availability(models.Model):
     STATUS = (
         ('Si', 'Si'),
         ('No', 'No'),
     )
     name = models.ForeignKey(Oficiales, null=True, on_delete=models.SET_NULL)
-    # game =
+    # game2 =
+    partido1 = models.CharField(max_length=100, null=True, choices=STATUS)
+    # game1 =
+    partido2 = models.CharField(max_length=100, null=True, choices=STATUS)
     date_informed = models.DateTimeField(auto_now_add=True, null=True)
-    status = models.CharField(max_length=100, null=True, choices=STATUS)
+    
+    def __str__(self):
+        return self.name
+    
+    
+# CALENDARIO
+class Calendar(models.Model):
+    HORARIO = (
+        ('1er partido - 13hs', '1er partido - 13hs'),
+        ('2do partido - 15.30hs', '2do partido - 15.30hs'),
+    )
+    EQUIPOS = (
+        ('Corsarios', 'Corsarios'),
+        ('Cruzados', 'Cruzados'),
+        ('Jabalies', 'Jabalies'),
+        ('Legionarios', 'Legionarios'),
+        ('Osos Polares', 'Osos Polares'),
+        ('Tiburones', 'Tiburones'),
+    )
+    fecha = models.DateField()
+    horario = models.CharField(max_length=100, null=True, choices=HORARIO)
+    local = models.CharField(max_length=100, null=True, choices=EQUIPOS)
+    visitante = models.CharField(max_length=100, null=True, choices=EQUIPOS)
+    
+    def __str__(self):
+        return '{} {}'.format(self.fecha, self.horario)
